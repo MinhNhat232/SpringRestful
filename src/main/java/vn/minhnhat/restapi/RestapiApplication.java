30a41340895d1dd267ab36b3cfa3b7b2cefd1948
Minh Nhat <minhkr2302@gmail.com>
2025-07-18 14:12:16 +0700

    Initial commit: Spring project

---
+++ src/main/java/vn/minhnhat/restapi/RestapiApplication.java
@@ -0,0 +1,15 @@
+package vn.minhnhat.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class })
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
